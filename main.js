f9b5aa44df7511db85242c0fb30a9a8e8b372b2f
Ripal Patel <ripal212007@gmail.com>
2018-09-21 13:01:20 -0500

    added style to student-form, student-card and introduction-card

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ const printToDom = (stringToPrint, divId) => {
 
 // function to display student form
 const buildNewStudentForm = () => {
-    let domString =`<div class="border border-light rounded bg-light w-75 m-auto">
+    let domString =`<div class="border border-light rounded bg-light w-75 m-auto student-form">
                         <div class="form-group mb-5 ml-5">
-                            <h3>Enter First Year's Name</h3>
+                            <h3 class="form-header">Enter First Year's Name</h3>
                         </div>
                         <form class="form-inline d-flex justify-content-center">
                             <p class="h4 mr-4">Student:</p>
                             <div class="form-group mb-2">
-                                <label for="staticEmail2" class="sr-only">Student</label>
+                                <label class="sr-only">Student</label>
                             </div>
                             <div class="form-group mx-sm-3 mb-2">
                                 <input type="text" class="form-control mr-5" id="student-input" placeholder="Name">
                             </div>
-                            <button type="submit" class="btn btn-primary mb-2 ml-3 pr-5 pl-5" id="sort-button">Sort!</button>
+                            <button type="submit" class="btn btn-primary mb-2 ml-3 pr-5 pl-5 btn-info" id="sort-button">Sort!</button>
                         </form>
                     </div>`
     printToDom(domString, 'student-form');
@@ -32,11 +32,11 @@ const buildStudentCard = () => {
     const studentInput = document.getElementById("student-input").value; 
     // collect random house variable
     const randHouse = house[Math.floor(Math.random()*house.length)];
-    let cardString = `<div class="card m-2">
+    let cardString = `<div class="card m-2 student-card ">
                         <div class="card-body">
-                            <h5 class="card-title">${studentInput}</h5>
+                            <h5 class="card-title student-name">${studentInput}</h5>
                             <p class="card-text">${randHouse}</p>
-                            <button href="#" class="btn btn-danger expelButton">Expel</button>
+                            <button href="#" class="btn btn-danger pr-5 pl-5 expelButton">Expel</button>
                         </div>
                       </div>`;
     counter++;
